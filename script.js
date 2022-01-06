9241fe16c063cd1b1f8cb183bd70df41c4c7350a
Leonel <leox-23@hotmail.com>
2022-01-06 18:15:04 -0500

    pantalla 1 movil lista, sidebar y link styles corregidos

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-$('.btn').on("click", function(){
-    $('.btn').toggleClass('close-btn');
-    $('.sidebar').toggleClass('sidebar-open');
-    if ($("#btn").hasClass("close-btn")) {
-        $(this).attr('src', 'assets/shared/icon-close.svg')
-    } else {
-        $(this).attr('src', 'assets/shared/icon-hamburger.svg')
-    }
-  });
-
+$(".btn").on("click", function () {
+  $(".btn").toggleClass("close-btn");
+  $(".sidebar").toggleClass("sidebar-open");
+  if ($("#btn").hasClass("close-btn")) {
+    $(this).attr("src", "assets/shared/icon-close.svg");
+  } else {
+    $(this).attr("src", "assets/shared/icon-hamburger.svg");
+  }
+});

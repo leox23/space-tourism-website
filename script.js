e87137920dcdacb77bef92959675a99074392826
Leonel <leox-23@hotmail.com>
2022-01-11 19:46:03 -0500

    correccion en lectura de json y escritura de home desde JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 /*
 ######################################################################
                Sidebar function
@@ -18,12 +19,32 @@ $(".btn").on("click", function () {
                Reading JSON file
 ######################################################################
 */ 
-function extractJSON(tasksList, callbackFunc){
+var homeObj = {}, destinationsObj = {}, crewObj = {}, technologyObj = {}, subHeadingsVec = {}; 
+var actualPage = 0
+function extractJSON(tasksList , callbackFunc){
     const request = new XMLHttpRequest();
     request.addEventListener('readystatechange', () => {
         if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            callbackFunc(undefined, data);
+          const data = JSON.parse(request.responseText);
+          /*
+          ! tengo que trabajar desde adentro, no hay forma de exportar las variables desde funcion arrow
+          home = data.home[0]
+          destinations = data.destinations
+          crew = data.crew
+          technology = data.technology
+          subHeadings = data.subHeadings[0]
+          console.log("desde dentro de la funcion")
+          console.log(home)
+          console.log(destinations)
+          console.log(crew)
+          console.log(technology)
+          console.log(subHeadings)
+          */
+
+          if (actualPage == 0){
+            writeHome(data.home[0],data.subHeadings[0].home)
+          }
+          
         } else if (request.readyState === 4) {
             callbackFunc('no se han podido obtener los datos', undefined);
         }
@@ -34,16 +55,25 @@ function extractJSON(tasksList, callbackFunc){
 
 /*
 ######################################################################
-         breaking down JSON variables
+         00 Home
 ######################################################################
 */
-//TODO tengo que agregar la misma mecanica a la pagina -> meterlo en json
-function declare(dataJSON) {
-  homeObj = dataJSON.home
-  destinationsObj = dataJSON.destinations
-  crewObj = dataJSON.crew
-  technologyObj = dataJSON.technology
-  subHeadingsVec = dataJSON.subHeadings
+function writeHome(home, subHeading){
+  console.log("desde dentro de write home")
+  console.log(home)
+  console.log(subHeading)
+
+subHead = document.getElementById("textBeforeTitle")
+subHead.textContent = subHeading
+
+title = document.getElementById("main-title")
+title.textContent = home.title
+
+mainText = document.getElementById("textAfterTitle")
+mainText.textContent = home.mainText
+
+button = document.getElementById("exploreText")
+button.textContent = home.bugButtonText
 }
 
 /*

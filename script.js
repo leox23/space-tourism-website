afac4a7aeeb095967ff4515d845993f6585e39b9
Leonel <leox-23@hotmail.com>
2022-01-26 01:35:40 -0500

    generar las paginas desde js casi completo, falta colocar stilos en de pagina destination en clases aparte, para poder funcionar correctamten

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /*
 ######################################################################
                Sidebar function
@@ -28,6 +27,7 @@ function extractJSON(tasksList , callbackFunc){
           const data = JSON.parse(request.responseText);
           /*
           ! tengo que trabajar desde adentro, no hay forma de exportar las variables desde funcion arrow
+          
           home = data.home[0]
           destinations = data.destinations
           crew = data.crew
@@ -39,10 +39,32 @@ function extractJSON(tasksList , callbackFunc){
           console.log(crew)
           console.log(technology)
           console.log(subHeadings)
-          */
 
+          */
+         
           if (actualPage == 0){
-            writeHome(data.home[0],data.subHeadings[0].home)
+            //esto es para la pagina home
+            writeHomePage(data.home[0],data.subHeadings[0].home)
+          }
+          if (actualPage == 1){
+            //esto es para la pagina home
+            //writeHome(data.home[0],data.subHeadings[0].home)
+            
+            //este es de la pagina 2
+            writeDestinationPage(data.destinations[0],data.subHeadings[0].destination)
+            destinationEspecific(data.destinations[0])
+          }
+          else if (actualPage == 1.0){
+            destinationEspecific(data.destinations[0])
+          }
+          else if (actualPage == 1.1){
+            destinationEspecific(data.destinations[1])
+          }
+          else if (actualPage == 1.2){
+            destinationEspecific(data.destinations[2])
+          }
+          else if (actualPage == 1.3){
+            destinationEspecific(data.destinations[3])
           }
           
         } else if (request.readyState === 4) {
@@ -58,10 +80,18 @@ function extractJSON(tasksList , callbackFunc){
          00 Home
 ######################################################################
 */
-function writeHome(home, subHeading){
+function writeHomePage(home, subHeading){
   console.log("desde dentro de write home")
   console.log(home)
   console.log(subHeading)
+//hidding
+document.getElementById("planetImg").style.display = "none"
+document.getElementById("containerTabs").style.display = "none"
+document.getElementsByClassName("containerDescriptionDestinations")[0].style.display = "none"
+document.getElementsByClassName("containerDescriptionDestinations")[1].style.display = "none"
+
+//showing
+boxButton = document.querySelector(".explore").style.display = "flex"
 
 subHead = document.getElementById("textBeforeTitle")
 subHead.textContent = subHeading
@@ -74,25 +104,89 @@ mainText.textContent = home.mainText
 
 button = document.getElementById("exploreText")
 button.textContent = home.bugButtonText
+//! tengo que agregar el boton de nuevo pero creandolo desde aqui
+
+
 }
 
 /*
 ######################################################################
           01  DESTINATIONS
 ######################################################################
-*//* 
+*/
+function writeDestinationPage(destinations, subHeading){
+//hidding
+document.querySelector(".explore").style.display = "none"
+
+//showing
+document.getElementById("planetImg").style.display = "inherit "
+document.getElementById("containerTabs").style.display = "flex"
+document.getElementsByClassName("containerDescriptionDestinations")[0].style.display = "flex"
+document.getElementsByClassName("containerDescriptionDestinations")[1].style.display = "flex"
+
+
+
 textBeforeTitle = document.getElementById("textBeforeTitle")
-textBeforeTitle.innerHTML = "<span>01</span> Pick your destination"
- */
-//inserta imagen del astro
+textBeforeTitle.innerHTML = "<b>01 </b> " + subHeading
+//fondo                                           
+document.body.style.background = 'url("assets/destination/background-destination-mobile.jpg") no-repeat';
+//todo no funciona opacarlo a lo mejor tendre que ponerlo en una etiquita img
+//document.body.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+
+document.getElementById("containerTabs").innerHTML = `
+<a href="#" onclick="actualPage = 1.0; extractJSON('data.json', (err, data) => {});return false;" class="destinationTabs">Moon</a>
+<a href="#" onclick="actualPage = 1.1; extractJSON('data.json', (err, data) => {});return false;" class="destinationTabs">Mars</a>
+<a href="#" onclick="actualPage = 1.2; extractJSON('data.json', (err, data) => {});return false;" class="destinationTabs">Europa</a>
+<a href="#" onclick="actualPage = 1.3; extractJSON('data.json', (err, data) => {});return false;" class="destinationTabs">Titan</a>
+`
+
 /*
-var newImg = document.createElement("img")
-var srcIMG = document.
+//creando linea horizontal
+let pbase = document.querySelector("#textAfterTitle");
+let lineHr = document.createElement("hr");
+pbase.after(hr);
 
 */
+descriptionData = document.querySelectorAll(".descriptionData")[0]
+descriptionData.textContent = "Avg. distance"
+
+descriptionData = document.querySelectorAll(".descriptionData")[1]
+descriptionData.textContent = "Est. Travel Time"
+
+
+}
+
+function destinationEspecific(destinations) {
+//cambiando imagen del planeta
+document.getElementById("planetImg").src= destinations.images.webp
+//document.getElementsByTagName("H1")[0].setAttribute("class", "democlass");
+
+//colocando nombre del planeta  
+title = document.getElementById("main-title")
+title.textContent = destinations.name 
+
+//colocando contenido
+mainText = document.getElementById("textAfterTitle")
+mainText.textContent = destinations.description
+
+//distancia
+dataPlanet = document.querySelectorAll(".dataDestination")[0]
+dataPlanet.textContent = destinations.distance
+
+//tiempo de recorrido
+dataPlanet = document.querySelectorAll(".dataDestination")[1]
+dataPlanet.textContent = destinations.travel
 
 
 
+$(".btn").toggleClass("close-btn");
+$(".sidebar").toggleClass("sidebar-open");
+if ($("#btn").hasClass("close-btn")) {
+  $(this).attr("src", "assets/shared/icon-close.svg");
+} else {
+  $(this).attr("src", "assets/shared/icon-hamburger.svg");
+}
+}
 
 
 
